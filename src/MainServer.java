7a7259fbc655903632e9b642e48235457b0a028b
frankenjoy123 <frankenjoy123@gmail.com>
2017-08-20 16:42:35 +0800

    first init

---
+++ src/MainServer.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class MainServer {
+
+	public static void main(String[] args) throws IOException {
+		MyNIORector reacot=new MyNIORector(9000);
+		reacot.start();
+	}
+
+}
